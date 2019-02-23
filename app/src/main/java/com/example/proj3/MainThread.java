20b1f1cca247620f13112984f4993c55ee912bd2
GartDev <garvdl@yahoo.com>
2019-02-22 19:52:43 -0500

    Set up game loop for animation and game events

--- app/src/main/java/com/example/proj3/MainThread.java
+++ app/src/main/java/com/example/proj3/MainThread.java
@@ -3,7 +3,34 @@ package com.example.proj3;
 import android.view.SurfaceHolder;
 
 public class MainThread extends Thread {
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+
     public MainThread(SurfaceHolder surfaceHolder, GameView gameView){
-        super();
+            super();
+            this.surfaceHolder = surfaceHolder;
+            this.gameView = gameView;
+    }
+    @Override
+    public void run(){
+        while(running){
+            canvas = null;
+
+            try{
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized(surfaceHolder){
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+            }catch (Exception e) {} finally{
+                if (canvas!= null){
+                    try{
+                        surfaceHolder.unlockcanvasAndPost(canvas);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
