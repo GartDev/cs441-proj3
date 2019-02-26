18b9d0cbf30f7210e5bba55aa169ef4acb201e46
GartDev <gartdev@Garrett.localdomain>
2019-02-26 17:56:38 -0500

    loaded all sprites into game, began hitbox work

--- app/src/main/java/com/example/proj3/CharacterSprite.java
+++ app/src/main/java/com/example/proj3/CharacterSprite.java
@@ -5,12 +5,27 @@ import android.graphics.Canvas;
 
 public class CharacterSprite {
     private Bitmap image;
-
+    public int x,y,xright,ytop;
     public CharacterSprite(Bitmap bmp){
         image = bmp;
+        x = 100;
+        y = 100;
+    }
+    public CharacterSprite(Bitmap bmp,int xloc, int yloc, int xsize, int ysize){
+        image = bmp;
+        x = xloc;
+        y = yloc;
+        xright = xloc + xsize;
+        ytop = yloc - ysize;
+
     }
 
     public void draw(Canvas canvas){
-        canvas.drawBitmap(image, 100, 100, null);
+        canvas.drawBitmap(image, x, y, null);
+    }
+
+    public void update(int xvel,int yvel){
+        y += yvel;
+        x += xvel;
     }
 }
--- app/src/main/java/com/example/proj3/GameView.java
+++ app/src/main/java/com/example/proj3/GameView.java
@@ -1,6 +1,7 @@
 package com.example.proj3;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -11,6 +12,13 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
     private CharacterSprite jonny;
+    private CharacterSprite goal;
+    private int xVelocity = 5;
+    private int yVelocity = 5;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private CharacterSprite ball;
+    private CharacterSprite garbage;
 
     public GameView(Context context) {
         super(context);
@@ -27,14 +35,21 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder) {
         thread.setRunning(true);
         thread.start();
-        jonny = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.jonnyblue));
+        jonny = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.jonnyblue),((screenWidth/2)-50),(screenHeight-100),100,100);
+        goal = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.goal),((screenWidth/2)-100),screenHeight/8,200,75);
+        ball = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.ball),((screenWidth/2)-25),screenHeight/2,50,50);
+        garbage = new CharacterSprite(BitmapFactory.decodeResource(getResources(),R.drawable.garbage),((screenWidth/4)-32),screenHeight/2,75,75);
+
     }
     @Override
     public void draw(Canvas canvas){
         super.draw(canvas);
         if(canvas != null){
-            canvas.drawColor(Color.WHITE);
+            canvas.drawColor(Color.GREEN);
             jonny.draw(canvas);
+            goal.draw(canvas);
+            ball.draw(canvas);
+            garbage.draw(canvas);
         }
     }
     @Override
@@ -51,6 +66,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
     public void update(){
+        jonny.update(5,-5);
+        if(((jonny.x >= ball.x)&&(jonny.x <= ball.xright))||((jonny.xright >= ball.x)&&(jonny.xright <= ball.xright))){
+            if(((jonny.y >= ball.ytop)&&(jonny.y <= ball.y))||((jonny.ytop >= ball.ytop)&&(jonny.ytop <= ball.y))){
 
+            }
+        }
     }
 }
